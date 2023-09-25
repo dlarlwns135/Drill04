e9e2818624f23a1274a46c86db88f79340c862dd
dlarlwns135 <dlarlwns135@naver.com>
2023-09-25 15:44:04 +0900

    상하좌우 이동 완료
    ㅇ

--- move_character_with_key.py
+++ move_character_with_key.py
@@ -11,28 +11,39 @@ character = load_image('animation_sheet.png')
 
 def handle_events():
     global running
-    global x, y
-    # fill here
+    global dir_x, dir_y
 
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
             running = False
-        elif event.type == SDL_MOUSEMOTION:
-            x, y = event.x, TUK_HEIGHT - 1 - event.y
-        elif event.type == SDL_KEYDOWN and event.key == SDLK_ESCAPE:
-            running = False
-        # fill here
+        elif event.type == SDL_KEYDOWN:
+            if event.key == SDLK_RIGHT:
+                dir_x += 1
+            elif event.key == SDLK_LEFT:
+                dir_x -= 1
+            elif event.key == SDLK_UP:
+                dir_y += 1
+            elif event.key == SDLK_DOWN:
+                dir_y -= 1
+
+            elif event.key == SDLK_ESCAPE:
+                running = False
+        elif event.type == SDL_KEYUP:
+            if event.key == SDLK_RIGHT:
+                dir_x -= 1
+            elif event.key == SDLK_LEFT:
+                dir_x += 1
+            elif event.key == SDLK_UP:
+                dir_y -= 1
+            elif event.key == SDLK_DOWN:
+                dir_y += 1
 
 
 running = True
 frame = 0
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
-hide_cursor()
-
-# fill here
-
-
+dir_x, dir_y = 0, 0
 
 while running:
     clear_canvas()
@@ -42,7 +53,10 @@ while running:
 
     update_canvas()
     handle_events()
-    frame = (frame + 1) % 8
+    if dir != 0:
+        frame = (frame + 1) % 8
+        x += dir_x * 5
+        y += dir_y * 5
     delay(0.05)
 
 close_canvas()
