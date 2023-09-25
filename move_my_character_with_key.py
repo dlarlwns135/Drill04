312ec89ac7d24c1692a8e44f76482b077f469c90
dlarlwns135 <dlarlwns135@naver.com>
2023-09-25 16:26:36 +0900

    상하좌우 이미지까지 완성
    good

--- move_my_character_with_key.py
+++ move_my_character_with_key.py
@@ -4,14 +4,15 @@ from pico2d import *
 # fill here
 
 TUK_WIDTH, TUK_HEIGHT = 1280, 1024
+SIZE = 100
 open_canvas(TUK_WIDTH, TUK_HEIGHT)
 tuk_ground = load_image('TUK_GROUND.png')
 # character = load_image('animation_sheet.png')
 character = load_image('TEST1.png')
 
 def handle_events():
-    global running
-    global dir_x, dir_y
+    global running, moving
+    global dir_x, dir_y, rear
 
     events = get_events()
     for event in events:
@@ -20,15 +21,20 @@ def handle_events():
         elif event.type == SDL_KEYDOWN:
             if event.key == SDLK_RIGHT:
                 dir_x += 1
+                rear = 1
             elif event.key == SDLK_LEFT:
                 dir_x -= 1
+                rear = 2
             elif event.key == SDLK_UP:
                 dir_y += 1
+                rear = 0
             elif event.key == SDLK_DOWN:
                 dir_y -= 1
+                rear = 3
 
             elif event.key == SDLK_ESCAPE:
                 running = False
+            moving = True
         elif event.type == SDL_KEYUP:
             if event.key == SDLK_RIGHT:
                 dir_x -= 1
@@ -38,26 +44,38 @@ def handle_events():
                 dir_y -= 1
             elif event.key == SDLK_DOWN:
                 dir_y += 1
+            moving = False
 
 
 running = True
 frame = 0
 x, y = TUK_WIDTH // 2, TUK_HEIGHT // 2
 dir_x, dir_y = 0, 0
+moving = False
+rear = 3
 
 while running:
     clear_canvas()
 
     tuk_ground.draw(TUK_WIDTH // 2, TUK_HEIGHT // 2)
+
+    if moving == False:
+        character.clip_draw(0, rear * 64, 64, 64, x, y, SIZE, SIZE)
+    else:
+        character.clip_draw(frame * 64, rear * 64, 64, 64, x, y, SIZE, SIZE)
     # character.clip_draw(frame * 100, 100 * 1, 100, 100, x, y)
-    character.clip_draw(frame * 64, 0, 64, 64, x, y)
+    # if move_mode == 0:
+    #     character.clip_draw(0, rear*64, 64, 64, x, y,SIZE,SIZE)
+    # else:
+    #     character.clip_draw(frame * 64, 0, SIZE, SIZE, x, y,SIZE,SIZE)
     update_canvas()
     handle_events()
+
     if dir != 0:
         frame = (frame + 1) % 4
         x += dir_x * 5
         y += dir_y * 5
-    delay(0.1)
+    delay(0.05)
 
 close_canvas()
 
